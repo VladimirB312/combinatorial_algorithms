e97d3861d6a27680a3bc3455fa23ce4b7e5eda4d
VladimirB312 <158708298+VladimirB312@users.noreply.github.com>
2025-02-12 17:08:05 +0300

    Update permutations.cpp

--- lab1/permutations/permutations.cpp
+++ lab1/permutations/permutations.cpp
@@ -9,7 +9,7 @@
 #include <chrono>
 #include <boost/multiprecision/cpp_int.hpp>
 
-int calcFinallyMatrix(std::vector<std::vector<int>>& distance, std::vector<std::vector<int>>& weight, const size_t N,
+int CalcFinallyMatrix(std::vector<std::vector<int>>& distance, std::vector<std::vector<int>>& weight, const size_t N,
                 std::vector<int>& perm) {
     int sumFlow = 0;
     for (int i = 0; i < N; i++) {
@@ -21,7 +21,7 @@ int calcFinallyMatrix(std::vector<std::vector<int>>& distance, std::vector<std::
     return sumFlow;
 }
 
-std::vector<int> getMinPermutation(std::vector<std::vector<int>>& distance, std::vector<std::vector<int>>& weight, const size_t N) {
+std::vector<int> GetMinPermutation(std::vector<std::vector<int>>& distance, std::vector<std::vector<int>>& weight, const size_t N) {
     std::vector<int> perm(N), minPerm(N);
     for (int i = 0; i < N; i++) {
         perm[i] = i;
@@ -32,7 +32,7 @@ std::vector<int> getMinPermutation(std::vector<std::vector<int>>& distance, std:
 
     std::sort(perm.begin(), perm.end());
     do {
-        int currSumFlow = calcFinallyMatrix(distance, weight, N, perm);
+        int currSumFlow = CalcFinallyMatrix(distance, weight, N, perm);
         if (minSumFlow > currSumFlow) {
             minSumFlow = currSumFlow;
             minPerm = perm;
@@ -44,7 +44,7 @@ std::vector<int> getMinPermutation(std::vector<std::vector<int>>& distance, std:
     return minPerm;
 }
 
-std::vector<std::vector<int>> getMatrixFromFile(std::ifstream& inFile) {
+std::vector<std::vector<int>> GetMatrixFromFile(std::ifstream& inFile) {
     int size = 0;
     inFile >> size;
     std::vector<std::vector<int>> matrix(size, std::vector<int>(size));
@@ -57,7 +57,7 @@ std::vector<std::vector<int>> getMatrixFromFile(std::ifstream& inFile) {
     return matrix;
 }
 
-boost::multiprecision::cpp_int calcFact(boost::multiprecision::cpp_int dur, size_t N, size_t target) {
+boost::multiprecision::cpp_int CalcFact(boost::multiprecision::cpp_int dur, size_t N, size_t target) {
     boost::multiprecision::cpp_int time = dur;
     for (size_t i = N + 1; i <= target; i++) {
         time *= i;
@@ -66,17 +66,16 @@ boost::multiprecision::cpp_int calcFact(boost::multiprecision::cpp_int dur, size
     return time;
 }
 
-void printTimeForOtherN(boost::multiprecision::cpp_int timeInMSeconds, size_t N) {
-    std::cout << "\nIn msec = " << timeInMSeconds << "\n";
-    boost::multiprecision::cpp_int time = calcFact(timeInMSeconds, N, 15) / 1000 / 60 / 60;
+void PrintTimeForOtherN(boost::multiprecision::cpp_int timeInMSeconds, size_t N) {
+    boost::multiprecision::cpp_int time = CalcFact(timeInMSeconds, N, 15) / 1000 / 60 / 60;
     std::cout << "\nFor N = 15: " << time << " hours\n";
-    time = calcFact(time, 15, 20) / 24 / 365;
+    time = CalcFact(time, 15, 20) / 24 / 365;
     std::cout << "\nFor N = 20: " << time << " years\n";
-    time = calcFact(time, 20, 50) / 1000000000;
+    time = CalcFact(time, 20, 50) / 1000000000;
     std::cout << "\nFor N = 50: " << time << " billion years\n";
 }
 
-void calculateMinFlow()
+void CalculateMinFlow()
 {
     std::ifstream distanceFile("distance.txt");
     if (!distanceFile.is_open()) {
@@ -90,12 +89,12 @@ void calculateMinFlow()
         return;
     }
 
-    std::vector<std::vector<int>> distance = getMatrixFromFile(distanceFile);
-    std::vector<std::vector<int>> weight = getMatrixFromFile(weightFile);
+    std::vector<std::vector<int>> distance = GetMatrixFromFile(distanceFile);
+    std::vector<std::vector<int>> weight = GetMatrixFromFile(weightFile);
 
     const size_t N = distance[0].size();
     auto start = std::chrono::high_resolution_clock::now();
-    std::vector<int> minPerm = getMinPermutation(distance, weight, N);
+    std::vector<int> minPerm = GetMinPermutation(distance, weight, N);
     auto end = std::chrono::high_resolution_clock::now();
     std::cout << "Placement: ";
     copy(minPerm.begin(), minPerm.end(), std::ostream_iterator<int>(std::cout, " "));
@@ -105,10 +104,10 @@ void calculateMinFlow()
 
     boost::multiprecision::cpp_int timeInMSeconds = static_cast<unsigned long long>(dur.count() * 1000);
     
-    printTimeForOtherN(timeInMSeconds, N);    
+    PrintTimeForOtherN(timeInMSeconds, N);    
 }
 
 int main() {
-    calculateMinFlow();
+    CalculateMinFlow();
     return 0;
 }
